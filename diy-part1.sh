e818ce77e96162f111ebef55f7fe66d941c3d212
mslovecc <33970221+mslovecc@users.noreply.github.com>
2020-10-31 20:04:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,8 +15,8 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-chmod +x $GITHUB_WORKSPACE/lede/copy-sunxi.sh
-$GITHUB_WORKSPACE/lede/copy-sunxi.sh
+chmod +x $GITHUB_WORKSPACE/lede/copy-amd64.sh
+$GITHUB_WORKSPACE/lede/copy-amd64.sh
 mkdir package/ming
 git clone https://github.com/mslovecc/luci-app-vlmcsd.git package/ming/luci-app-vlmcsd
 git clone https://github.com/mslovecc/openwrt-vlmcsd.git package/ming/openwrt-vlmcsd
